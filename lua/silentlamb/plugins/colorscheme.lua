0abfb065f236434a88531a013bdec7cd49e86050
Marcin Gliński <undefinedlamb@gmail.com>
2024-03-12 09:41:35 +0100

    colorscheme: add transparency

--- lua/silentlamb/plugins/colorscheme.lua
+++ lua/silentlamb/plugins/colorscheme.lua
@@ -1,14 +1,21 @@
 return {
   'folke/tokyonight.nvim',
---  'shaunsingh/moonlight.nvim',
---  "olimorris/onedarkpro.nvim",
+  --  'shaunsingh/moonlight.nvim',
+  --  "olimorris/onedarkpro.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-     vim.cmd.colorscheme 'tokyonight-night'
-     -- vim.cmd.colorscheme 'onedark'
-     -- vim.cmd.colorscheme 'onedark'
-     
-  end
-}
+    require('tokyonight').setup {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    }
 
+    vim.cmd.colorscheme 'tokyonight-night'
+
+    -- vim.cmd.colorscheme 'onedark'
+    -- vim.cmd.colorscheme 'onedark'
+  end,
+}
